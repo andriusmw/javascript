cea88e78aa85c247f3f7b88fe7d8638ca432ec6c
andriusmw <andriusmw@yahoo.es>
2022-04-18 10:36:49 +0200

    alert-prompt

---
+++ dia-1/alert-prompt.js
@@ -0,0 +1,7 @@
+"use strict";
+
+alert("Hey Esto es una alerta");
+const nombre = prompt("Cuál es tu nombre?");
+
+const edad = prompt("Cuál es tu edad?");
+console.log("EL NOMBRE ES: " + nombre + " La edad es: " + edad);
