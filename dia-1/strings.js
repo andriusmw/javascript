6a1e2383e9356f44ae86762d141a73d970acbd5a
andriusmw <andriusmw@yahoo.es>
2022-04-18 11:01:38 +0200

    strings y arrays

--- dia-1/strings.js
+++ dia-1/strings.js
@@ -9,3 +9,17 @@ const nombre = nombreCompleto.substring(0, 4);
 const apellido = nombreCompleto.substring(4, 13);
 
 console.log("nombre: " + nombre + " apellido: " + apellido);
+
+/************************** split y arrays---------------------------- */
+
+const listaGrutas = "naranja,pera,manzana,mandarina";
+const arraycontrocitos = listaGrutas.split(",");
+//Separa por comas y devuelve un array.
+console.log(arraycontrocitos);
+
+const posicionmanzana = listaGrutas.indexOf("manzana");
+//Busca su índice
+console.log(posicionmanzana);
+//.lastIndexOf
+
+console.log(listaGrutas.lastIndexOf("manzana"));
