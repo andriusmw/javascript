287e0cfd95e79c9283187a7f7648f04317f64f39
andriusmw <andriusmw@yahoo.es>
2022-04-18 09:28:44 +0200

    iniciando la clase

---
+++ dia-1/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//hace que el javascript sea más seguro y salten errores , que sea
+//más restictivo. impide malas prácticas, por ejemplo trabajar con una variable
+//vacía o sin declarar primero.
+
+console.log("Hola Mundo");
+
+console.log(2 + 3);
+
+8 + 7; //literales, no son variables ni constantes, son valores
