94a64ca6e2b2f46cdbacfa6c0c008565b20796ab
andriusmw <andriusmw@yahoo.es>
2022-04-18 10:01:22 +0200

    tipos de datos

--- dia-1/main.js
+++ dia-1/main.js
@@ -27,3 +27,48 @@ console.log(nombre);
 
 var edad = 18;
 //acceso a ellas en cualquier parte del scope y se pueden cambiar;no se recomienda su uso, se prefiere let.
+
+/*------------------------- TIPOS DE VALORES --------------------------- */
+/*
+/* ----------------------------------------------------------------------*/
+
+//Strings
+
+let surname = "rodriguez";
+surname = "gonzalez";
+console.log(surname);
+
+//Number
+let age = 18;
+console.log(age);
+
+//BigInts
+
+//Se le pone una n al final del número.
+//Sirve para poder operar con números muy grandes sin perder precision
+// 5454545645454545n
+
+//Boolean
+
+true;
+false;
+
+const isFull = true;
+//Se le suele poner nombre de pregunta a los booleanos
+const isGameOver = false;
+
+//Null
+
+const notasAdicionales = null;
+//Algo está vacío a posta, tiene el valor nulo.
+
+//Undefinied
+
+let colorFavorito;
+console.log(colorFavorito); //Esto va a dar undefinied, NO defnido, valor
+//no asignado.
+
+//Symbol
+let sym1 = Symbol("foo");
+let sym2 = Symbol("foo");
+//Aunque el contenido sea el mismo, son datos distintos.
