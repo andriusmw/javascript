b15f2bdcbe2e4ae6c7b34fb7a3dc7a62d8ee7299
andriusmw <andriusmw@yahoo.es>
2022-04-18 09:39:18 +0200

    const y let

--- dia-1/main.js
+++ dia-1/main.js
@@ -8,3 +8,22 @@ console.log("Hola Mundo");
 console.log(2 + 3);
 
 8 + 7; //literales, no son variables ni constantes, son valores
+
+/*----------------------------CONSTANTES Y VARIABLES ----------------------*/
+/*
+/*-------------------------------------------------------------------------*/
+
+//Camel case --> siempre todo junto sin espacios empezando por minusculas y
+//separando cada palabra por mayusuculas
+
+const numeroDeCoches = 2;
+//mejor usar const siempre que se pueda, salvo que sepas que su valor va a cambiar, precisamente para evitar que sea un let y que se cambie en algún momento sin darte cuenta. Así prevenimos fallos de comportamiento.
+
+console.log(numeroDeCoches);
+
+let nombre = "andrius";
+
+console.log(nombre);
+
+var edad = 18;
+//acceso a ellas en cualquier parte del scope y se pueden cambiar;no se recomienda su uso, se prefiere let.
