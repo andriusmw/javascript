951c438cd1ece544c02b38c22c4814f3e8599a2d
andriusmw <andriusmw@yahoo.es>
2022-04-19 11:14:58 +0200

    scopes

--- dia-2/js/scopes.js
+++ dia-2/js/scopes.js
@@ -3,6 +3,9 @@
 //SCOPES
 //Se puede acceder de dentro a fuera a variables globales, pero no al revés. Por eso se pasan los valores de dentro a variables de fuera, para poder ser accesibles.
 
+//var declara las variables a nivel global aunque se declaren dentro
+//por eso se usan let y const
+
 const a = 5;
 
 {
