ce41d7cfd7b1b32198d965dc751ef56ce0aa0579
andriusmw <andriusmw@yahoo.es>
2022-04-19 10:21:00 +0200

    condiconales if else

--- dia-2/js/condicionales.js
+++ dia-2/js/condicionales.js
@@ -4,5 +4,9 @@ const a = 2;
 const b = 4;
 
 if (a < b) {
-  console.log("Hye");
+  console.log("A es menor que B");
+} else if (a > b) {
+  ("A es mayor que B");
+} else {
+  console.log("A y B son iguales");
 }
