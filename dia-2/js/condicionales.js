6756260d88aa069c2f791a9073a9dfe521d5f87b
andriusmw <andriusmw@yahoo.es>
2022-04-19 10:28:31 +0200

    condiconales if else mostrar resultados

--- dia-2/js/condicionales.js
+++ dia-2/js/condicionales.js
@@ -1,12 +1,14 @@
 "use strict";
 
-const a = 2;
-const b = 4;
+const a = +prompt("Introduce A");
+const b = +prompt("Introduce B");
+//El + delante del prompt comvierte a número
 
 if (a < b) {
   console.log("A es menor que B");
 } else if (a > b) {
-  ("A es mayor que B");
+  console.log("A es mayor que B");
+  //console.log(´${a} es mayor que ${b}´);
 } else {
   console.log("A y B son iguales");
 }
