2b4f06978c3de721714f354c27c0bdc43ced93a2
andriusmw <andriusmw@yahoo.es>
2022-04-19 09:24:35 +0200

    comparadores

--- dia-2/js/operadores.js
+++ dia-2/js/operadores.js
@@ -8,3 +8,18 @@ console.log(2 * 2);
 console.log(2 / 2);
 console.log(2 % 2);
 console.log(2 ** 2); //Eleva a 2
+
+let clicks = 0;
+
+clicks = clicks + 1;
+//clicks++;
+//clicks--;
+
+// OPERADORES DE COMPARACIÓN
+
+// === !== == != < <= > >=
+
+const nombre = "samu";
+
+console.log(nombre === "pepe");
+//Devuelve false
