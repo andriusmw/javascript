5d0761f9fc5ff79b2d2e59b2eb57e56dc7ea99e4
andriusmw <andriusmw@yahoo.es>
2022-04-19 13:25:29 +0200

    recorrer strings

--- dia-2/js/bucles.js
+++ dia-2/js/bucles.js
@@ -4,13 +4,26 @@
 
 let a = 0;
 
-while (a < 5) {
+while (a <= 5) {
   console.log(a);
   a++;
 }
 
 // FOR
-
-for (let b = 0; b < 5; b++) {
+let contador = 0;
+for (let b = 0; b <= 5; b++) {
   console.log(b);
+  contador++;
+}
+
+console.log("contador= " + contador);
+
+// Recorrer strings
+
+const nombre = "Pepito";
+
+console.log(nombre[1]);
+
+for (let i = 0; i < nombre.length; i++) {
+  console.log(nombre[i]);
 }
