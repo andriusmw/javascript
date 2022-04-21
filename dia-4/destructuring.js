ff2bd91d8ec87f2dcf00762cd19e60f0fbcd9a6d
andriusmw <andriusmw@yahoo.es>
2022-04-21 11:05:15 +0200

    destructuring2

--- dia-4/destructuring.js
+++ dia-4/destructuring.js
@@ -1,5 +1,6 @@
 "user strict";
 
+//Objetos
 const user = {
   nombre: "pepe",
   apellido: "gonzalez",
@@ -19,5 +20,18 @@ const { nombre: nombre, apellido: apellido, edad: edad } = user;
 //la propiedd y entonces se guarda en una variable del mismo nombre
 //que la propiedad.
 
+/*
+
+const { nombre, apellido, edad} = user;
+*/
+
 console.log(user);
 console.log(nombre, apellido, edad);
+
+//Arrays
+
+const peliculas = ["peli1", "peli2", "peli3", "peli4"];
+
+const [a, b, c, d] = peliculas;
+//Guarda los valores en las variables, según su posición
+//la variable a con indice 0, tiene el valor peli1
