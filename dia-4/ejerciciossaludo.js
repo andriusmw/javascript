33dec7cc0a667d0aa3d75747d73196f911497d62
andriusmw <andriusmw@yahoo.es>
2022-04-21 14:02:20 +0200

    ejsaludo

---
+++ dia-4/ejerciciossaludo.js
@@ -0,0 +1,18 @@
+"use strict";
+const nombre = prompt("Indica tu nombre");
+const idioma = prompt("indica tu idioma");
+
+const saludo = (a, b) => {
+  switch (b) {
+    case "ES":
+      return "Hola " + a;
+    case "EN":
+      return "Hello " + a;
+    case "GL":
+      return "Ola " + a;
+    default:
+      "Idioma no disponible";
+  }
+};
+
+console.log(saludo(nombre, idioma));
