a5a78acdc76b4930d164c85e88eef6f819cc56c3
andriusmw <andriusmw@yahoo.es>
2022-04-25 10:22:15 +0200

    callbacks2

--- dia-5/arrays-methods.js
+++ dia-5/arrays-methods.js
@@ -1,3 +1,17 @@
 "use strict";
 
-[2, 7, 1, 8, 9, 2];
+const numeros = [2, 7, 1, 8, 9, 2];
+
+const mapear = (callback) => {
+  for (const numero of numeros) {
+    callback(numero);
+  }
+};
+//introducimos la función restar3 y luego dentro del bucle, le pasamos de parámetro el número de cada iteración y se ejecuta restar3
+//que nos devuelve el valor de ese número - 3.
+
+const restar3 = (num) => {
+  return num - 3;
+};
+
+mapear(restar3);
