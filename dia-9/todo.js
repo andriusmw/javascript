1b7e1f5efd444f0eca34b38bba8195a65cbf5529
andriusmw <andriusmw@yahoo.es>
2022-05-02 11:00:43 +0200

    ejerciciotodolist1

---
+++ dia-9/todo.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const descip = document.getElementById(descrip).textContent;
+
+const ul = document.getElementById("lista");
+let li = document.createElement("li");
+li.appendChild(ul);
+
+//falta el evento y la llamada, meter en una funcion
