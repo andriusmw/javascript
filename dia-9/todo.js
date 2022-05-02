3520c7d39cea283290b0de4022c09e8060a44129
andriusmw <andriusmw@yahoo.es>
2022-05-02 13:09:32 +0200

    local storage con array vacio para cuando recarga

--- dia-9/todo.js
+++ dia-9/todo.js
@@ -1,19 +1,31 @@
 "use strict";
-const boton = document.getElementById("boton");
 
-boton.addEventListener("click", (event) => {
+/******************INICIO************************* */
+const form = document.querySelector("form");
+
+const tareas = JSON.parse(localStorage.getItem("tasks")) || [];
+//Esto lee el localstorage y le pasa el array a tareas; sino hay nada, crea array vacío.
+
+const ul = document.getElementById("lista");
+
+for (const taskDescrip of tareas) {
+  const li = document.createElement("li");
+  li.textContent = taskDescrip;
+  ul.appendChild(li);
+}
+
+/**************** CREAR TAREAS ************************ */
+
+form.addEventListener("submit", (event) => {
   {
     event.preventDefault();
     const descrip = document.getElementById("descrip").value;
 
-    const ul = document.getElementById("lista");
     let li = document.createElement("li");
     li.textContent = descrip;
     ul.appendChild(li);
 
     /***********local storage********** */
-
-    const tareas = [];
 
     //añadir datos
     tareas.push(li.textContent);
